aaf596d9c19ec7b7877b10ef1c957ffb11db1c4f
Jared Hanson <jaredhanson@gmail.com>
2021-12-31 14:21:23 -0800

    Add tests.

--- test/test.test.js
+++ test/test.test.js
@@ -17,12 +17,67 @@ describe('Test', function() {
       .request(function(page) {
         expect(page).to.be.an.instanceof(Page);
       })
-      .finish(function() {
-        done();
-      })
+      .finish(done)
       .generate();
     }); // should invoke sync callback
     
-  });
+    it('should invoke async callback', function(done) {
+      chai.kerouac.use(function(page, next) {
+        page.end();
+      })
+      .request(function(page, cb) {
+        expect(page).to.be.an.instanceof(Page);
+        cb();
+      })
+      .finish(done)
+      .generate();
+    }); // should invoke sync callback
+    
+  }); // #request
+  
+  describe('#finish', function() {
+  
+    it('should invoke callback', function(done) {
+      chai.kerouac.use(function(page, next) {
+        page.end();
+      })
+      .finish(function() {
+        expect(this).to.be.an.instanceof(Page);
+        done();
+      })
+      .generate();
+    }); // should invoke callback
+  
+  }); // #finish
+  
+  describe('#next', function() {
+  
+    it('should invoke callback', function(done) {
+      chai.kerouac.use(function(page, next) {
+        next();
+      })
+      .next(function(err, page) {
+        expect(this).to.be.an.instanceof(Test);
+        expect(err).to.be.undefined;
+        expect(page).to.be.an.instanceof(Page);
+        done();
+      })
+      .generate();
+    }); // should invoke callback
+    
+    it('should invoke callback with error', function(done) {
+      chai.kerouac.use(function(page, next) {
+        next(new Error('something went wrong'));
+      })
+      .next(function(err, page) {
+        expect(this).to.be.an.instanceof(Test);
+        expect(err).to.be.an.instanceof(Error);
+        expect(page).to.be.an.instanceof(Page);
+        done();
+      })
+      .generate();
+    }); // should invoke callback with error
+  
+  }); // #next
   
 });
